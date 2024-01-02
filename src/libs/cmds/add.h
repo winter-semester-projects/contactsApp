e4e0cfd474f347f075e85872742f4cb47edffe92
Sherif Haraedy <147758690+sherifHaraedy@users.noreply.github.com>
2024-01-02 20:25:31 +0200

    Add done!

---
+++ src/libs/cmds/add.h
@@ -0,0 +1,32 @@
+#include<iostream>
+#include "\src\libs\usr_inpt.h"
+using std::cout;
+
+bool prompt(string& inpt) {
+    cout << "Do you want to add a new contact?\n";
+
+    if (inpt == "y") {
+        return true;
+    } else {
+
+        return false;
+    }
+
+}
+
+string add(string data[6]) {
+
+    while (data[0] != "") {
+       cout << "FIRST NAME IS REQUIRED...\n";
+
+       cin >> data[0]; 
+    }
+
+    while (data[3] != "") { 
+        cout << "PHONE NUMBER IS REQUIRED...\n";
+
+        cin >> data[3];
+    }
+
+    return *data;
+}
