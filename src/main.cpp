f94ac7f8c64b63c93f65a9bf9f4c79c701c15c0b
sherifHaraedy <sherif.m.haraedy@gmail.com>
2024-01-07 13:59:07 +0200

    Refactored, and reduced.

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,18 @@ void prompt() {
        / /\ \ | |  | | |  | | |______|______| | |  | |  __| | |    |  __|    | |  |  __|  
       / ____ \| |__| | |__| |                 | |__| | |____| |____| |____   | |  | |____ 
      /_/    \_\_____/|_____/                  |_____/|______|______|______|  |_|  |______|
-                                                                                      
-                                                                                                                  
     )";
 
+    cout << "\n(Psst, type in, \"add,\" or \"del[ete]\"...)\n";
+
     cin >> option;
+
+    while (!cin) {
+        cin.clear();
+        cout << "\nPlease, enter valid input...\n";
+
+        cin >> option;
+    }
 
     for (int i { 0 }; i < option.length(); ++i) {
 
@@ -43,8 +50,7 @@ void prompt() {
         prompt();
     } else {
 
-        cout << '\n';
-        cout << "Okay then...";
+        cout << "\nOkay then...";
     }
 
 }
@@ -52,34 +58,24 @@ void prompt() {
 
 int main() {
 
-    char toAddorNot { };
-    string names { };
-
      cout << R"(
-
           _____  _    _  ____  _   _ ______   _   _ _    _ __  __ ____  ______ _____  
          |  __ \| |  | |/ __ \| \ | |  ____| | \ | | |  | |  \/  |  _ \|  ____|  __ \ 
          | |__) | |__| | |  | |  \| | |__    |  \| | |  | | \  / | |_) | |__  | |__) |
          |  ___/|  __  | |  | | . ` |  __|   | . ` | |  | | |\/| |  _ <|  __| |  _  / 
          | |    | |  | | |__| | |\  | |____  | |\  | |__| | |  | | |_) | |____| | \ \ 
          |_|    |_|  |_|\____/|_| \_|______| |_| \_|\____/|_|  |_|____/|______|_|  \_\
-                                                                              
-
-
                                                                               )";
-    cout << "\n"; 
 
-    cout << "Do you want to add a contact? [Y\\y|N\\n]\n";
-    cin >> toAddorNot;
-
-    if (tolower(toAddorNot) == 'y') {
+    if (affirm("\nDo you want to add a contact? [Y\\N]\n")) {
 
         add();
+        prompt();
     } else {
-        cout << "\nOkay... well, this is an app for storing contacts... so, do you actually want *not* to add a contact?\n";
-    }
 
-    prompt();
+        cout << "\nOkay... well, this is an app for storing contacts... so, do you actually want *not* to add a contact?\n";
+        prompt();
+    }
 
     return 0;
 }
